bdc6b8469dcbd05998a25a3bc013e4d6e75e6fac
Bach Nguyen <ngb1998@gmail.com>
2024-02-02 20:06:17 +0900

    analyzer: add EAR chart visualize

--- study-stream/src/components/StatsTile.vue
+++ study-stream/src/components/StatsTile.vue
@@ -1,8 +1,8 @@
 <template>
-  <div :class="chatIsOpen ? 'stats-wrapper open' : 'stats-wrapper'">
+  <div :class="statsIsOpen ? 'stats-wrapper open' : 'stats-wrapper'">
     <div>
-      <button class="stats" @click="handleChatClick">
-        <template v-if="chatIsOpen">
+      <button class="stats" @click="handleStatsClick">
+        <template v-if="statsIsOpen">
           <img class="icon" :src="close" alt="" />
         </template>
         <template v-else>
@@ -12,11 +12,13 @@
     </div>
 
     <div class="stats-container">
-      <div id="chart">
-        <apexchart ref="chart" type="line" height="350" :options="chartOptions" :series="series"></apexchart>
+      <div id="attention-chart">
+        <apexchart ref="attentionChart" type="line" height="350" :options="attentionChartOptions" :series="attentionSeries"></apexchart>
       </div>
 
-      <!-- <button @click="test">test</button> -->
+      <div id="EAR-chart">
+        <apexchart ref="EARChart" type="line" height="350" :options="EARChartOptions" :series="EARSeries"></apexchart>
+      </div>
 
     </div>
   </div>
@@ -36,14 +38,11 @@ export default {
   props: ["sendMessage", "messages"],
   data() {
     return {
-      // last_x : 0,
-      // last_y : 0,
-      chatIsOpen: false,
+      statsIsOpen: false,
       close,
       stats,
-      // statsUrl: 'http://localhost:3000',
 
-      series: [{
+      attentionSeries: [{
           name: "attention score",
           data: [
             {
@@ -51,9 +50,8 @@ export default {
               y: 0
             }
           ], 
-          // [0.05 , 0.205, 0.175, 0.255, 0.245, 0.31 , 0.345, 0.455, 0.74 ], //[10, 41, 35, 51, 49, 62, 69, 91, 148]
       }],
-      chartOptions: {
+      attentionChartOptions: {
         chart: {
           height: 350,
           type: 'line',
@@ -88,10 +86,64 @@ export default {
           },
         },
         xaxis: {
-          // categories: [], 
-          //['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
           type: 'datetime',
-          range: 120000, //777600000, //5,
+          range: 120000, // ms
+          labels: {
+            format: 'HH:mm',
+          }
+        },
+        yaxis: {
+          min: 0,
+          max: 1
+        }
+      },
+
+      EARSeries: [{
+          name: "EAR",
+          data: [
+            {
+              x: 0,
+              y: 0
+            }
+          ], 
+      }],
+      EARChartOptions: {
+        chart: {
+          height: 350,
+          type: 'line',
+          zoom: {
+            enabled: false
+          },
+          toolbar: {
+            show: false
+          },
+          animations: {
+            enabled: true,
+            easing: 'linear',
+            dynamicAnimation: {
+              speed: 1000
+            }
+          },
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: 'smooth'
+        },
+        title: {
+          text: 'Eye-Aspect-Ratio (EAR)',
+          align: 'left'
+        },
+        grid: {
+          row: {
+            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+            opacity: 0.5
+          },
+        },
+        xaxis: {
+          type: 'datetime',
+          range: 120000, // ms
           labels: {
             format: 'HH:mm',
           }
@@ -110,35 +162,21 @@ export default {
     var count = 0;
 
     window.setInterval(async function () {
-      var data = [];
+      var attentionData = [];
+      var EARData = [];
       const statsUrl = "http://localhost:3000";
-      // var x = [];
-      // var y = [];
-      var timestamps = []; 
       var date = "";
-      // var rand = (Math.random() * (1.00 - 0.00)).toFixed(2);
 
-      // if (isNaN(this.last_x)){this.last_x = 0}
-      // if (isNaN(this.last_y)){this.last_y = 0}
-
-      // if (count < 10){
-      //   this.last_x = this.last_x+500; //86400000;
-      //   this.last_y = rand; //this.last_y+rand;
-      //   // console.log(this.last_x);
-      //   // console.log(this.last_y);
-      // }
-      // var last_x = this.last_x;
-      // var last_y = this.last_y;
-      // count = count+1;
 
       // update data series
       await axios({
         method: 'GET',
         url: statsUrl + "/data/",
       }).then(function(response) {
-        console.log(response);
+        // console.log(response);
         response.data.data.forEach(el => {
           var prob = el['prob'];
+          var earVal = el['EAR_cur'];
           var time = el['time'].split(' ')[1];
           var hour = parseInt(time.split(':')[0]);
           var min = parseInt(time.split(':')[1]);
@@ -146,58 +184,46 @@ export default {
           var time_ms = toMilliseconds(hour, min, sec);
           date = el['time'].split(' ')[0];
 
-          data.push({
-            x: time_ms, //last_x, //parseInt(el['id']),
-            y: prob, //el['prob']
+          attentionData.push({
+            x: time_ms, 
+            y: prob, 
+          });
+
+          EARData.push({
+            x: time_ms, 
+            y: earVal, 
           });
         });
       });
 
-      // console.log(data);
-      // // update categories
-      // await axios({
-      //   method: 'GET',
-      //   url: statsUrl + "/time/",
-      // }).then(function(response) {
-        
-      //   response.data.forEach(el => {
-      //     timestamps.push(el['time'].split(" ")[1]);
-      //     // data.push(parseInt(el['id']) );
-      //   });
 
-      //   date = response.data[0]['time'].split(" ")[0];
-
-      // });
-
-      me.$refs.chart.updateSeries([{
+      me.$refs.attentionChart.updateSeries([{
         name: 'attention score',
-        data: data
+        data: attentionData
       }]);
 
-      me.$refs.chart.updateOptions({
+      me.$refs.attentionChart.updateOptions({
         title: {
           text: 'Attention Performance - ' + date
         },
-        // xaxis: {
-        //   // categories: timestamps,
-        //   range: 5,
-        // }
+      });
+
+      me.$refs.EARChart.updateSeries([{
+        name: 'EAR',
+        data: EARData
+      }]);
+
+      me.$refs.EARChart.updateOptions({
+        title: {
+          text: 'Eye-Aspect-Ratio (EAR) - ' + date
+        },
       });
     }, 3000)
   },
 
   methods: {
-
-    // Toggle chat's view (open/closed)
-    handleChatClick() {
-      this.chatIsOpen = !this.chatIsOpen;
-    },
-    // Send chat message using prop method from CallTile.vue
-    submitForm(e) {
-      e.preventDefault();
-
-      this.sendMessage(this.text);
-      this.text = "";
+    handleStatsClick() {
+      this.statsIsOpen = !this.statsIsOpen;
     },
   },
   
@@ -239,52 +265,7 @@ button.stats {
   top: calc(50% - 150px);
   right: 548px;
 }
-.messages {
-  flex: 1;
-  padding-right: 32px;
-}
-.input {
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  align-items: flex-start;
-}
-label {
-  color: #fff;
-}
-.input textarea {
-  width: 100%;
-  border: none;
-  resize: none;
-  font-family: "Ropa Sans", sans-serif;
-  font-size: 16px;
-}
-.input textarea::placeholder {
-  font-family: "Ropa Sans", sans-serif;
-  font-size: 16px;
-}
-form {
-  display: flex;
-  border-bottom: 2px solid #c8d1dc;
-}
 
-.submit-button {
-  padding: 4px;
-  margin: 0 0 0 16px;
-  border: none;
-  background-color: #fff;
-}
-
-.chat-message {
-  color: #121a24;
-  text-align: left;
-  font-size: 14px;
-  line-height: 18px;
-  margin: 0 0 20px;
-}
-.chat-message .chat-name {
-  color: #6b7785;
-}
 
 @media screen and (max-width: 700px) {
   .stats-container {
